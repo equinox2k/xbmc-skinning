a9d7328f4d08e00d251304b51edc129ff22c71a7
Peter.Tribe <Peter.Tribe@683f067c-5321-0410-a0ed-6b430fcc5097>
2006-11-07 11:23:41 +0000

    Added Editor

--- Plug-Ins/Editor/Editor/My Project/Settings.Designer.vb
+++ Plug-Ins/Editor/Editor/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.SkinEditor.Plugin.Calculator.My.MySettings
+        Friend ReadOnly Property Settings() As Global.SkinEditor.Plugin.Editor.My.MySettings
             Get
-                Return Global.SkinEditor.Plugin.Calculator.My.MySettings.Default
+                Return Global.SkinEditor.Plugin.Editor.My.MySettings.Default
             End Get
         End Property
     End Module
